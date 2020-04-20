2654d1b0ff4268abf328ea6d0653c3466e9f6f2f
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2020-04-20 20:25:08 +0530

    final commit completed 1 and 2 question

---
+++ src/Lab01FunctionsArray.java
@@ -0,0 +1,43 @@
+public class Lab01FunctionsArray {
+    int[] arr;
+    int[] temp;
+    int visited = -1;
+
+    public Lab01FunctionsArray(int[] arr) {
+        this.arr = arr;
+        temp = new int[arr.length];
+    }
+
+    public void duplicateElements() {
+
+        for (int i = 0; i < arr.length; i++) {
+            int count = 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    temp[j] = visited;
+                }
+            }
+            if (temp[i] != visited)
+                temp[i] = count;
+        }
+        printElement();
+    }
+
+    private void printElement() {
+        System.out.println("Occurrence of each element : ");
+        for (int i = 0; i < temp.length; i++) {
+            if (temp[i] != visited)
+                System.out.println(arr[i] + "    --->    " + temp[i]);
+        }
+    }
+
+    public void duplicateElementsPrint() {
+        System.out.print("Duplicate Elements are : ");
+        for (int i = 0; i < temp.length; i++) {
+            if (temp[i] == visited) {
+                System.out.print(arr[i] + "  ");
+            }
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Lab01FunctionsArray array = new Lab01FunctionsArray(new int[]{1, 3, 5, 2, 3, 5, 2});
+        array.duplicateElements();
+        array.duplicateElementsPrint();
+    }
+}
