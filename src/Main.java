4dd0d5116c613c0e5afc5ba4af07ca0653fa8b27
Mayanksingh2004 <mayank.singh1_cs18@gla.ac.in>
2020-04-20 20:40:27 +0530

    final commit completed 1 and 2 question

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         Lab01FunctionsArray array = new Lab01FunctionsArray(new int[]{1, 3, 5, 2, 3, 5, 2});
         array.duplicateElements();
         array.duplicateElementsPrint();
+
     }
 }
